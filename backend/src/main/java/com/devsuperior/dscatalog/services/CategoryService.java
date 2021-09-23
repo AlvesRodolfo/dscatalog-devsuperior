9ff174febb6a7d6b1199f37e4f553358f1e14c92
Rodolfo Alves <rodolfoalveseng@gmail.com>
2021-09-23 20:19:15 -0300

    Database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/devsuperior/dscatalog/services/CategoryService.java
@@ -2,8 +2,11 @@ package com.devsuperior.dscatalog.services;
 
 import java.util.List;
 
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
@@ -16,6 +19,7 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
+	@Transactional(readOnly = true) //readOnly melhora a performance, não trava o BD --> operações somente de leitura
 	public List<Category> findAll(){
 		return repository.findAll();
 	}
